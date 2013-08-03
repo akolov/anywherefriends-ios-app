aacbf93b0b14a382529ed7207f7af6d611fa6e2e
Alexander Kolov <a@piu.fm>
2013-08-04 00:07:12 +0200

    Initial Commit

---
+++ AnywhereFriends/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AnywhereFriends
+//
+//  Created by Alexander Kolov on 8/4/13.
+//  Copyright (c) 2013 Anywherefriends. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AWFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AWFAppDelegate class]));
+  }
+}
---
+++ AnywhereFriendsTests/AnywhereFriendsTests.m
@@ -0,0 +1,34 @@
+//
+//  AnywhereFriendsTests.m
+//  AnywhereFriendsTests
+//
+//  Created by Alexander Kolov on 8/4/13.
+//  Copyright (c) 2013 Anywherefriends. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnywhereFriendsTests : XCTestCase
+
+@end
+
+@implementation AnywhereFriendsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
