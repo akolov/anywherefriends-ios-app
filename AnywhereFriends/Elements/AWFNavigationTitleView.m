b7a498261cb49ec11b4611fc51ea9d76bb917c94
Alexander Kolov <a@piu.fm>
2013-08-07 14:26:01 +0200

    Adjust font descender

--- AnywhereFriends/Elements/AWFNavigationTitleView.m
+++ AnywhereFriends/Elements/AWFNavigationTitleView.m
@@ -13,7 +13,6 @@
 + (instancetype)navigationTitleView {
   AWFNavigationTitleView *view = [[AWFNavigationTitleView alloc] initWithFrame:CGRectZero];
   [view sizeToFit];
-  [view setFrameHeight:22.0f];
   return view;
 }
 
